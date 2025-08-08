fcd6d75db221513537b6a8a8daf62291d3df486c
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2025-08-08 15:36:42 -0400

    fix: Export WebpageVideo class

--- src/index.js
+++ src/index.js
@@ -10,3 +10,4 @@ export { WebpageMeta } from "./webpage-meta.js";
 export { WebpageFeed } from "./webpage-feed.js";
 export { WebpageImage } from "./webpage-image.js";
 export { WebpageFavicon } from "./webpage-favicon.js";
+export { WebpageVideo } from "./webpage-video.js";
